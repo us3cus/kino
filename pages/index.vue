85c0acc9d5d51c80e3697e293a74c4385036fb2b
motya <motya228228@mail.ru>
2024-12-02 10:30:40 +0500

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h1>Welcome to the homepage</h1>
+    <AppAlert>
+      This is an auto-imported component
+      <button type="button" class="btn btn-outline-warning">Warning</button>
+    </AppAlert>
+  </div>
+</template>
